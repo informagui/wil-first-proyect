0cfde7a753048df30aed7186ba6ce350a09b9fb5
Magali <magali.krueel@gmail.com>
2023-03-25 16:04:49 -0300

    Agregando los files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+let saveEl = document.getElementById("guardados")
+
+function save(){
+    saveEl.textContent += count + "-"
+    count = 0
+    countEl.textContent = count
+}
